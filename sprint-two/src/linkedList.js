30baf70a40edea15423947a6209cef8a692e46b2
Aaron Mikulka <aaronjmikulka@gmail.com>
2022-10-27 13:30:40 -0600

    started linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,27 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //add a node of a specific value to the tail position
+    let newNode = Node(value);
+    //add a pointer from the previous tail pointing @ the new tail (in the Next property)
+
+//***********************
+
+    //initialize run head must change from null to new node
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    }
+    //change list.tail to point to the new tail
+
   };
 
   list.removeHead = function() {
+
   };
 
   list.contains = function(target) {
+
   };
 
   return list;
