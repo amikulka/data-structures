f5ffc26834560157c207b0750e3805e03be0ba39
Aaron Mikulka <aaronjmikulka@gmail.com>
2022-10-28 14:39:13 -0600

    completed tree, binary-search-tree, and hashTable

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,57 @@
 var BinarySearchTree = function(value) {
+  let obj = Object.create(BinarySearchTree.prototype);
+  obj.value = value;
+  obj.left = null;
+  obj.right = null;
+  return obj;
 };
+
+BinarySearchTree.prototype.insert = function (value) {
+  //vale is less than the current node value
+  if (value < this.value) {
+    //child node left is not null
+    if (this.left) {
+      this.left.insert(value);
+    } else {
+      this.left = new BinarySearchTree(value);
+    }
+  } else if (value > this.value) {
+    if (this.right) {
+      this.right.insert(value);
+    } else {
+      this.right = new BinarySearchTree(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function (target) {
+  if (this.value === target) {
+    return true;
+  } else if (target < this.value) {
+    if (this.left) {
+      return this.left.contains(target);
+    } else {
+      return false;
+    }
+  } else {
+    if (this.right) {
+      return this.right.contains(target);
+    } else {
+      return false;
+    }
+  }
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (callbackFunction) {
+  callbackFunction(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(callbackFunction);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(callbackFunction);
+  }
+};
+
 
 
 /*
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,16 +5,58 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  //withing the limited array [storage]
+  //within the bucket
+  //iterate the tuples
+  //if the key is not found
+  //create the key value pair
+  // let bucket = this._storage.get(index);
+  // bucket = bucket || [];
+  if (!this._storage.get(index)) {
+    this._storage.set(index, [[key, value]]);
+  } else {
+    let isFound = false;
+    for (let i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === key) {
+        this._storage.get(index)[i][1] = value;
+        isFound = true;
+      }
+    }
+    if (!isFound) {
+      this._storage.get(index).push([key, value]);
+    }
+  }
 };
 
-HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.retrieve = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  if (!this._storage.get(index)) {
+    return undefined;
+  } else {
+    let result;
+    this._storage.get(index).forEach(function (tuple) {
+      if (tuple[0] === key) {
+        result = tuple[1];
+      }
+    });
+    return result;
+  }
+
 };
 
-HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.remove = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  if (this._storage.get(index)) {
+    let tupleIndex;
+    this._storage.get(index).forEach(function (tuple, currentIndex) {
+      if (tuple[0] === key) {
+        tupleIndex = currentIndex;
+      }
+    });
+    this._storage.get(index).splice(tupleIndex, 1);
+  }
 };
 
 
